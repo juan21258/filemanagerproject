45d1ebf6418bd56b3db84c7deba6937ddf522e26
Juan <juan21258@hotmail.com>
2017-07-29 22:32:05 -0500

    Application basics complete

--- filemanagerog/app/controllers/archivos_controller.rb
+++ filemanagerog/app/controllers/archivos_controller.rb
@@ -1,4 +1,13 @@
 class ArchivosController < ApplicationController
+	#Seguridad o autenticacion
+	http_basic_authenticate_with name: "admin", password: "secret",
+	 except: [:index, :create, :new, :show]
+ 
+  def index
+    @articles = Article.all
+  end
+
+	#Definiciond de metodos CRUD(Create, Read, Update, Delete)
 	def index
     	@archivos = Archivo.all
   	end
@@ -8,18 +17,41 @@ class ArchivosController < ApplicationController
   	end
 
 	def new
+		@archivo = Archivo.new
+	end
 
+	def edit
+  		@archivo = Archivo.find(params[:id])
 	end
 
 	def create
-		render plain: params[:archivo].inspect
 		@archivo = Archivo.new(archivo_params)
 
-  		@archivo.save
-  		redirect_to @archivo
+		if @archivo.save
+    		redirect_to @archivo
+  		else
+    		render 'new'
+  		end  		
 	end
 
+	def update
+  		@archivo = Archivo.find(params[:id])
+ 
+  		if @archivo.update(archivo_params)
+    		redirect_to @archivo
+  		else
+    		render 'edit'
+  		end
+	end
 
+	def destroy
+  		@archivo = Archivo.find(params[:id])
+  		@archivo.destroy
+ 
+  		redirect_to archivo_path
+	end
+
+	#Acceso privado
 	private
   	def archivo_params
     	params.require(:archivo).permit(:title, :filetype, :datefile)
--- filemanagerog/app/models/archivo.rb
+++ filemanagerog/app/models/archivo.rb
@@ -1,2 +1,8 @@
 class Archivo < ApplicationRecord
+	validates :title, presence: true,
+                    length: { minimum: 1 }
+    validates :filetype, presence: true,
+                    length: { minimum: 1 }
+    validates :datefile, presence: true,
+                    length: { minimum: 4 }
 end
